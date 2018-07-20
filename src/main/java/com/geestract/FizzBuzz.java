2990370a73e44374f91f639c92f942910e84390e
glakshmi <glakshmi@gmail.com>
2018-07-20 18:43:36 +0530

    modified sources

--- src/main/java/com/geestract/FizzBuzz.java
+++ src/main/java/com/geestract/FizzBuzz.java
@@ -1,4 +1,4 @@
-package com.techprimers.testing;
+package com.geestract.testing;
 
 public class FizzBuzz {
 
--- src/test/java/com/geestract/FizzBuzzTest.java
+++ src/test/java/com/geestract/FizzBuzzTest.java
@@ -1,4 +1,4 @@
-package com.techprimers.testing;
+package com.geestract.testing;
 
 import org.junit.jupiter.api.*;
 
